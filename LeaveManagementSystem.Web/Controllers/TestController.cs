61abe15ac5d0a33083ca3f2830c9386291ac84c3
seraphion <djurdjevicster@gmail.com>
2026-07-11 12:39:13 +0200

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC Mastery",
+                DateOfBirth = new DateTime(1990, 1, 1)
+            };
+            return View(data);
+        }
+    }
+}
